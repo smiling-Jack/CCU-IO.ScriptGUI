77a6d6d87cc7edc69d750cea2c46886a2b47d6e1
smiling-Jack <Steffen.schorling@googlemail.com>
2014-02-20 16:35:13 +0100

    xs_select change to xs_combo

---
+++ js/xs_combo.js
@@ -0,0 +1,97 @@
+/** jQuery
+ *
+ *  xtreme simpel select
+ *
+ *  Copyright (c) 2014 Steffen Schorling http://github.com/smiling-Jack
+ *  MIT License (MIT)
+ *
+ */
+
+
+(function ($) {
+    $.fn.xs_combo = function (_options) {
+
+        if (typeof _options == "string") {
+            var text = $(this).children("span");
+            $(text).text(_options);
+            $(this).val(_options);
+            $(this).trigger("change");
+        } else if (_options == undefined) {
+            return $(this).val();
+        } else {
+
+            var $this = this;
+
+            var o = {
+                cssButton: _options.cssButton || "ui-widget ui-state-default ui-corner-all " + (_options.addcssButton || ""),
+                cssMenu: _options.cssMenu || "ui-widget-content ui-corner-all " + (_options.addcssMenu || ""),
+                cssFocus: _options.cssFocus || "ui-state-focus ui-corner-all " + (_options.addcssFocus || ""),
+                cssText: _options.cssText || "",
+                width: _options.cssFocus || false,
+                height: _options.height || false,
+                data: _options.data || [],
+                time: _options.time || 750,
+                val: _options.val || _options.data[0],
+                combo:_options.combo || true,
+            };
+
+            var liste = "";
+
+            var timer;
+            $.each(o.data, function () {
+                liste += ('<p class="' + o.cssText + '">' + this + '</p>')
+            });
+
+            this.addClass(o.cssButton);
+            this.append('<input readonly style="border: none; background-color: transparent;padding-top: 0;padding-bottom: 0"  type="text" value="' + o.val + '" class="' + o.cssText + '"></input>');
+            this.append('<div class="' + o.cssMenu + '">' + liste.toString() + '</div>');
+
+            this.find("div").hide();
+            text = this.children("input");
+            var list = this.children("div");
+            var list_elem = this.children("div").children("p");
+
+            $(list_elem)
+                .mouseenter(function () {
+                    $(this).addClass(o.cssFocus);
+                })
+                .mouseleave(function () {
+                    $(this).removeClass(o.cssFocus)
+                })
+                .click(function () {
+                    $($this).val($(this).text());
+                    $(text).val($(this).text());
+                    $($this).trigger("change");
+                    clearTimeout(timer);
+                });
+            $(this)
+                .mouseenter(function () {
+                    $(this).addClass(o.cssFocus)
+                })
+                .mouseleave(function () {
+                    $(this).removeClass(o.cssFocus)
+                })
+                .click(function () {
+                    clearTimeout(timer);
+                });
+
+            $(list)
+                .mouseenter(function () {
+                    clearTimeout(timer);
+                })
+                .mouseleave(function () {
+                    timer = setTimeout(function () {
+                        $(list).hide();
+                    }, o.time)
+                });
+
+            this.click(function () {
+                $(list).toggle();
+            })
+                .keydown(function(){
+                    $(list).hide();
+                })
+        }
+    }
+
+})(jQuery);
