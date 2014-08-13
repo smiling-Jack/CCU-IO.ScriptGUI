d04dcd4ec1a2121245a58691ad1fac6a9bddc0bc
smiling-Jack <Steffen.schorling@googlemail.com>
2014-08-13 12:20:02 +0200

    Add webkit Builder

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,21 @@
  */
 module.exports = function (grunt) {
 
+    grunt.loadNpmTasks('grunt-node-webkit-builder');
+
     grunt.registerTask('test', function ()  {
       console.log("hallo Welt")     ;
+    });
+
+    grunt.registerTask('build', function ()  {
+        grunt.initConfig({
+            nodewebkit: {
+                options: {
+                    platforms: ['win'],
+                    buildDir: 'webkitbuilds', // Where the build version of my node-webkit app is saved
+                },
+                src: ['src'] // Your node-webkit app
+            },
+        })
     })
 };
---
+++ node_modules/grunt-node-webkit-builder/node_modules/.bin/nwbuild.cmd
@@ -0,0 +1,5 @@
+@IF EXIST "%~dp0\node.exe" (
+  "%~dp0\node.exe"  "%~dp0\..\node-webkit-builder\bin\nwbuild" %*
+) ELSE (
+  node  "%~dp0\..\node-webkit-builder\bin\nwbuild" %*
+)
